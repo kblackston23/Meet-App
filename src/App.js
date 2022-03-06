4dca41bbff31e0dc1ed453d96068cd13b106fbfb
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-03-05 19:03:40 -0600

    Task 4.4 updating scenario 3 and adding api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,51 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
+import './nprogress.css';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events: events.slice(0, this.state.numberOfEvents), locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
+        });
+      }
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents />
-        <EventList />
+        <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents} />
       </div>
     )
   }
