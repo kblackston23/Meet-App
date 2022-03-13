0e46bfd1495f6e22901562b4c977ef20ffaf2c40
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-03-12 23:13:04 -0600

    trying to use pingdom

---
+++ build/precache-manifest.bdb23e373e0a80b10dbafeb804a848ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cca2b7c0f3563ba29ed",
+    "url": "/Meet-App/static/css/main.ce7b45da.chunk.css"
+  },
+  {
+    "revision": "9cca2b7c0f3563ba29ed",
+    "url": "/Meet-App/static/js/main.9cca2b7c.chunk.js"
+  },
+  {
+    "revision": "8033376cb70e04425994",
+    "url": "/Meet-App/static/js/1.8033376c.chunk.js"
+  },
+  {
+    "revision": "97bda1acba9ed9d12ab9",
+    "url": "/Meet-App/static/js/2.97bda1ac.chunk.js"
+  },
+  {
+    "revision": "04460397d35ddc96bea4",
+    "url": "/Meet-App/static/js/runtime~main.04460397.js"
+  },
+  {
+    "revision": "407645e5137d397e39b7c5b1b6c81a7f",
+    "url": "/Meet-App/index.html"
+  }
+];
