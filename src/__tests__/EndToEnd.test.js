666ebaadf862fb07b4dc1388c5244c96fdfde071
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-03-12 21:22:01 -0600

    minor update for end to end test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -20,19 +20,19 @@ describe('show/hide an event details', () => {
   });
 
   test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.event .event__Details');
+    const eventDetails = await page.$('.event .extra-details');
     expect(eventDetails).toBeNull();
   });
 
   test('User can expand an event to see its details', async () => {
-    await page.click('.event .details-btn');
-    const eventDetails = await page.$('.event .event__Details');
+    await page.click('.event .show-details');
+    const eventDetails = await page.$('.event .extra-details');
     expect(eventDetails).toBeDefined();
   });
 
   test('User can collapse an event to hide its details', async () => {
-    await page.click('.event .details-btn');
-    const eventDetails = await page.$('.event .event__Details');
+    await page.click('.event .hide-details');
+    const eventDetails = await page.$('.event .extra-details');
     expect(eventDetails).toBeNull();
   });
 });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.config('7e51457481574a5d8f70754632bf7df3').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
