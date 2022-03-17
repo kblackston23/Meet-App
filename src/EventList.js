9084f6eaed809a048452c85ddadb32457522e069
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-03-16 22:37:40 -0500

    Adding warning alert

--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Event from './Event';
+import { WarningAlert } from "./Alert";
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
       <ul className="EventList">
+        {!navigator.onLine ? (
+          <WarningAlert text="You are offline! The displayed event list has been loaded from the cache." />
+        ) : (
+          <WarningAlert text="" />
+        )}
         {events.map(event =>
           <li key={event.id}>
             <Event event={event} />
